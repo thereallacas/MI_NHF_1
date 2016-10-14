f221a13591b5b1ae0e5e77809249fbc7cbd1a6c1
Kyle Brandson <macbookpro@Macbooks-MacBook-Pro-2.local>
2016-10-15 01:22:28 +0200

    made the final touches. Will this pass the test??

--- NNSolutionFour.java
+++ NNSolutionFour.java
@@ -2,6 +2,18 @@ import java.util.ArrayList;
 
 public class NNSolutionFour {
 
+	private static double calculateAverage(ArrayList<Double> list) {
+		Double sum = 0.0;
+		if(!list.isEmpty()) {
+			for (Double mark : list) {
+				sum += mark;
+			}
+			return sum.doubleValue() / list.size();
+		}
+		return sum;
+	}
+
+
 	public static void main(String[] args) {
 		ScanInput sc = new ScanInput();
 		ArrayList<ArrayList<Double>> wbData = new ArrayList<>();
@@ -10,7 +22,9 @@ public class NNSolutionFour {
 		double learningFactor = learningParameters.get(1);
 		double ratioOfLearningInputs = learningParameters.get(2);
 
+
 		ArrayList<Double> architecture = sc.scan();
+		String arch = sc.theLine;
 		for (int i = 1; i < architecture.size(); i++) {
 			for (int j = 0; j < architecture.get(i); j++) {
 				wbData.add(new ArrayList<Double>(sc.scan()));
@@ -24,8 +38,9 @@ public class NNSolutionFour {
 		double nValidationInputs = nInputs - nLearningInputs;
 
 		for (int iepoch = 0; iepoch < epoch; iepoch++) {
+			ArrayList<Double> errorvectors = new ArrayList<Double>();
+
 			//teaching
-			
 			for (int i = 0; i < nLearningInputs; i++) {
 				ArrayList<Double> InandOut = sc.scan();
 				ArrayList<Double> inputs = new ArrayList<Double>(InandOut.subList(0, (int)(architecture.get(0)-0)));
@@ -36,48 +51,42 @@ public class NNSolutionFour {
 					for (int j = 0; j < outputs.size(); j++) {
 						epsilons.add(expectedoutputs.get(j)-outputs.get(j));
 					}
-					
-					System.out.println("Az "+(i+1)+". tanítóminta:");
 					nm.getDerivatives(epsilons);
 					nm.correctWeights(learningFactor);
-					
-					System.out.println("A módosult hálózat: ");
-					nm.printAll();
-					System.out.println();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}		
+			}
+			//validation
+			for (int i = 0; i < nValidationInputs; i++) {
+				ArrayList<Double> InandOut = sc.scan();
+				ArrayList<Double> inputs = new ArrayList<Double>(InandOut.subList(0, (int)(architecture.get(0)-0)));
+				ArrayList<Double> expectedoutputs = new ArrayList<Double>( InandOut.subList((int)(architecture.get(0)-0), InandOut.size()));
+
+				try {
+					ArrayList<Double> outputs = nm.getOutputs(inputs);
+					ArrayList<Double> epsilons = new ArrayList<Double>();
+					ArrayList<Double> errorvector = new ArrayList<Double>();
+					for (int j = 0; j < outputs.size(); j++) {
+						epsilons.add(expectedoutputs.get(j)-outputs.get(j));
+						errorvector.add(Math.pow((expectedoutputs.get(j)-outputs.get(j)),2));
+					}
+
+					double averageAsM = 0;
+					for (Double error : errorvector) {
+						averageAsM+=error;
+					}
+					averageAsM=averageAsM/outputs.size();
+					errorvectors.add(averageAsM);
 
 				} catch (Exception e) {
 					e.printStackTrace();
 				}		
 			}
-			
-			
-			//validation
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			//átlagos hiba
+			System.out.println(calculateAverage(errorvectors));
+			System.out.println(arch);
+			nm.printAll();
 		}
-
-
-
-
 	}
-
 }
